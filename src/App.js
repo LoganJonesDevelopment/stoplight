e3941506dfd91c6c4e871b23860aa1af3acef1cf
logan jones <nagol2012@yahoo.com>
2019-07-03 17:40:29 -0500

    initial push

--- src/App.js
+++ src/App.js
@@ -2,25 +2,54 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+   super(props);
+    this.state = {
+      greenClass: 'green',
+      yellowClass: 'yellowOff',
+      redClass: 'redOff'
+    }
+  }
+  
+ 
+  handleGreenClick = () => {
+   
+    this.setState({greenClass: !this.state.greenClass});
+            this.setState({yellowClass: 'yellowOff'});
+            this.setState({redClass: 'redOff'});
+
+
+    
+  }
+  handleYellowClick = () => {
+        this.setState({yellowClass: !this.state.yellowClass});
+ this.setState({greenClass: 'greenOff'});
+     this.setState({redClass: 'redOff'});
+  }
+  handleRedClick = () => {
+   this.setState({redClass: !this.state.redClass});
+     this.setState({greenClass: 'greenOff'});
+                this.setState({yellowClass: 'yellowOff'});
+
+
+  }
+  render() {
+     
+    return (
+      <div>
+        <h3> Click the stoplights to change them </h3>
+        <div class="container">  <br />
+        <div class={this.state.greenClass ? 'greenOff': 'green'} onClick= {this.handleGreenClick}></div> <br />
+        <div class={this.state.yellowClass ? 'yellowOff': 'yellow'} onClick={this.handleYellowClick}></div>  <br />
+         <div class={this.state.redClass ? 'redOff': 'red'} onClick={this.handleRedClick}></div>
+          
+        </div>
+        </div>
+    );
+  }
 }
+
+
 
 export default App;
